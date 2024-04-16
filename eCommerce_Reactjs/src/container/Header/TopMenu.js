79269bd86eeec6ba2dcd013b6c72a9d96c592fb4
Dang Dung <dungng12421@gmail.com>
2024-04-16 14:10:50 +0700

    ecom

--- eCommerce_Reactjs/src/container/Header/TopMenu.js
+++ eCommerce_Reactjs/src/container/Header/TopMenu.js
@@ -22,8 +22,8 @@ const TopMenu = props => {
                 <div className="row">
                     <div className="col-lg-7">
                         <div className="float-left">
-                            <p>Điện thoại: 0762216048 </p>
-                            <p>email: ptitshop@gmail.com</p>
+                            <p>Điện thoại: 0376829437 </p>
+                            <p>email: dungng12421@gmail.com</p>
                         </div>
                     </div>
                     <div className="col-lg-5">
--- eCommerce_Reactjs/src/container/System/Footer.js
+++ eCommerce_Reactjs/src/container/System/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
         <footer className="py-4 bg-light mt-auto">
             <div className="container-fluid px-4">
                 <div className="d-flex align-items-center justify-content-between small">
-                    <div className="text-muted">Bản quyền © Đỗ Tấn Thành 2022</div>
                     
                 </div>
             </div>
